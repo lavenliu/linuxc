e91c4176de41a2b6062c2a398a64e827707e00dc
LavenLiu <air.man.six@gmail.com>
2016-04-19 18:02:21 +0800

    add some C files

---
+++ my/network/chap02/2-4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int sock;
+	printf("fileno(stdin) = %d\n", fileno(stdin));
+	close(0);
+
+	/* sock will be zero, and it is not an error */
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	printf("sock = %d\n", sock);
+	
+    return 0;
+}
