dbebfee0b975b60f253ff372f7095d47f0264927
Laven Liu <air.man.six@gmail.com>
2017-05-01 10:38:25 +0800

    update

---
+++ sysinfo/my_uname_point.c
@@ -0,0 +1,28 @@
+#ifdef __linux__
+#define _GNU_SOURCE
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/utsname.h>
+
+int main(int argc, char *argv[])
+{
+    struct utsname *uts;
+
+    if (uname(uts) == -1) {
+        perror("uname");
+        exit(1);
+    }
+
+    printf("Node name:   %s\n", uts->nodename);
+    printf("System name: %s\n", uts->sysname);
+    printf("Release:     %s\n", uts->release);
+    printf("Version      %s\n", uts->version);
+    printf("Machine:     %s\n", uts->machine);
+#ifdef _GNU_SOURCE
+    printf("Domain name: %s\n", uts->domainname);
+#endif
+    
+    return 0;
+}
